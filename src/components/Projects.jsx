92c097fdf784756910b7cc7a7f367e90978f0ba4
RianneStreef <riannestreef@gmail.com>
2021-09-21 12:08:51 +0200

    projects background

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,11 +5,11 @@ const Projects = () => {
   return (
     <div id="projects" className="projects">
       <div className="content">
-        <h2>Projects</h2>
+        <h2 className="white-font">Projects</h2>
         <hr className="title-underline-white" />
-        <h3>Websites built by us</h3>
+        <h3 className="white-font">Websites built by us</h3>
         <div className="project-container">
-          <div className="project-card">Product description</div>
+          <div className="project-card white-font">Product description</div>
         </div>
       </div>
     </div>
