62f4ef2f283e07aaf2190a623ed0c78c420e722d
RianneStreef <riannestreef@gmail.com>
2021-09-22 09:09:21 +0200

    different about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,45 +4,49 @@ import "../styles/About.css";
 const About = () => {
   return (
     <div id="about" className="content about">
-      <h2>About Us</h2>
+      {/* <h2>About Us</h2>
       <hr className="title-underline" />
-      <h3> Graphic & Web Design</h3>
-
-      <p className="centered-text">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fringilla
-        venenatis sem, non eleifend ex tincidunt a. Phasellus blandit, leo id
-        finibus congue, orci felis elementum diam, ac venenatis mauris mi vitae
-        mi. Mauris imperdiet leo a molestie tincidunt. Etiam sed massa in lacus
-        egestas vehicula. Aliquam erat volutpat. Aliquam sollicitudin, eros ac
-        laoreet posuere, dolor sem facilisis sapien, ac venenatis nibh nibh
-        bibendum dolor. Vestibulum porttitor porta quam, consequat accumsan
-        tellus ultricies vel. Donec efficitur orci id turpis interdum lacinia.
-        Morbi neque mauris, tincidunt ultrices dui ac, dignissim venenatis
-        libero. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Phasellus in aliquet nisl. Ut at neque ac nunc molestie egestas. Class
-        aptent taciti sociosqu ad litora torquent per conubia nostra, per
-        inceptos himenaeos.
-      </p>
-
-      <p className="centered-text">
-        Praesent a bibendum nisi. Aliquam erat volutpat. Sed in convallis quam.
-        Nunc ultricies ipsum odio, vitae condimentum ex ullamcorper eu. Quisque
-        tincidunt, ex eu elementum iaculis, augue odio lacinia nisi, in
-        tincidunt ipsum lorem ut justo. Vivamus a porttitor lacus, in
-        consectetur neque. Morbi sit amet magna eget leo lobortis blandit. Morbi
-        vel arcu sed sem aliquet auctor.
-      </p>
-
-      <p className="centered-text">
-        Etiam rhoncus facilisis lorem, id auctor sapien porttitor ut. Duis porta
-        commodo tempor. Cras dignissim nisl eget lorem pretium euismod.
-        Curabitur blandit id arcu ut viverra. Mauris malesuada risus blandit
-        massa malesuada gravida. Suspendisse potenti. Quisque a lectus dictum,
-        varius lacus ac, vulputate lectus. Vestibulum consectetur nisi non quam
-        imperdiet auctor. Donec lacinia nec nibh et semper. Nunc ornare congue
-        nisi ac pulvinar. Ut luctus nisi dolor, et malesuada magna mattis eget.
-        Suspendisse interdum tellus maximus aliquam facilisis.
-      </p>
+      <h3> Graphic & Web Design</h3> */}
+      <div className="our-descriptions">
+        <div className="padding">
+          <h2>designer</h2>
+          <p className="centered-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+            fringilla venenatis sem, non eleifend ex tincidunt a. Phasellus
+            blandit, leo id finibus congue, orci felis elementum diam, ac
+            venenatis mauris mi vitae mi. Mauris imperdiet leo a molestie
+            tincidunt. Etiam sed massa in lacus egestas vehicula. Aliquam erat
+            volutpat. Aliquam sollicitudin, eros ac laoreet posuere, dolor sem
+            facilisis sapien, ac venenatis nibh nibh bibendum dolor. Vestibulum
+            porttitor porta quam, consequat accumsan tellus ultricies vel. Donec
+            efficitur orci id turpis interdum lacinia. Morbi neque mauris,
+            tincidunt ultrices dui ac, dignissim venenatis libero. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Phasellus in aliquet
+            nisl. Ut at neque ac nunc molestie egestas. Class aptent taciti
+            sociosqu ad litora torquent per conubia nostra, per inceptos
+            himenaeos.
+          </p>
+        </div>
+        <div className="padding">
+          <h2>&#60;coder&#62;</h2>
+          <p className="centered-text">
+            Praesent a bibendum nisi. Aliquam erat volutpat. Sed in convallis
+            quam. Nunc ultricies ipsum odio, vitae condimentum ex ullamcorper
+            eu. Quisque tincidunt, ex eu elementum iaculis, augue odio lacinia
+            nisi, in tincidunt ipsum lorem ut justo. Vivamus a porttitor lacus,
+            in consectetur neque. Morbi sit amet magna eget leo lobortis
+            blandit. Morbi vel arcu sed sem aliquet auctor. Etiam rhoncus
+            facilisis lorem, id auctor sapien porttitor ut. Duis porta commodo
+            tempor. Cras dignissim nisl eget lorem pretium euismod. Curabitur
+            blandit id arcu ut viverra. Mauris malesuada risus blandit massa
+            malesuada gravida. Suspendisse potenti. Quisque a lectus dictum,
+            varius lacus ac, vulputate lectus. Vestibulum consectetur nisi non
+            quam imperdiet auctor. Donec lacinia nec nibh et semper. Nunc ornare
+            congue nisi ac pulvinar. Ut luctus nisi dolor, et malesuada magna
+            mattis eget. Suspendisse interdum tellus maximus aliquam facilisis.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
